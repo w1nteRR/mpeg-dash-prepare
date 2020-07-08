264db91509c2faaeada18ef83b80d565ae988d15
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-08 20:08:03 +0300

    audio cut added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const electron = require('electron')
 const ffmpeg = require('fluent-ffmpeg')
+const fs = require('fs')
 
 require('electron-reload')(__dirname)
 
@@ -49,4 +50,51 @@ ipcMain.on('file-dialog-open', event => {
 			event.sender.send('file:metadata', metadata)
 		})
 	})
+})
+
+ipcMain.on('file:getAudio', async (event, audio) => {
+	const { file, streamNum, lang } = audio
+
+	const outputFile = file.replace(/^.*[\\\/]/, '').slice(0, -4)
+
+	try {
+
+		if(!fs.existsSync(`${__dirname}/converted/${outputFile}`)) {
+			await fs.promises.mkdir(`${__dirname}/converted/${outputFile}`)
+		}
+
+		if(fs.existsSync(`${__dirname}/converted/${outputFile}/${outputFile}_${lang}.aac`)) {
+			return event.sender.send('convertation:error', 'File already exists')
+		}
+
+		ffmpeg()
+			.input(file)
+			.outputOptions([
+				`-map 0:${streamNum}`, 
+				'-ac 2', 
+				'-ab 192k', 
+				'-vn', 
+				'-sn'
+			])
+			.output(`${__dirname}/converted/${outputFile}/${outputFile}_${lang}.aac`)
+
+			.on('start', () => event.sender.send('convertation:start'))
+			
+			.on('error', err => {
+				console.log(err.message)
+				event.sender.send('convertation:error', err.message)
+			})
+			
+			.on('progress', progress => {
+				console.log(progress.percent)
+				event.sender.send('convertation:processing', progress.percent)
+			})  
+			
+			.on('end', () => console.log('Finished processing'))
+			
+			.run() 
+
+	} catch (err) {
+		console.log(err)
+	}	
 })
