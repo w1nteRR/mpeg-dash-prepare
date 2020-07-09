1af8b4c1af0d8af69ad75cd52b440b7e9bf93004
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-09 14:49:11 +0300

    scss loader added

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,12 @@ const config = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [ 'style-loader', 'css-loader', 'sass-loader']
             }
-        ]
+        ],
     },
     resolve: {
         extensions: [".js", ".jsx"]
