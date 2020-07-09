244d475acb196da670ac0831f666f3ff84a70725
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-09 22:31:55 +0300

    card component refactored

--- src/components/shared/styled/Card.js
+++ src/components/shared/styled/Card.js
@@ -1,9 +1,24 @@
+import React from 'react'
 import styled from 'styled-components'
 
-export const Card = styled.div`
+import { Container } from '../layout'
+import { Text } from '../typography'
+
+const CardStyled = styled.div`
     padding: 10px;
-    width: 35%;
-    box-shadow: 0px 0px 5px rgba(0,0,0,0.67);
+    width: ${props => props.w || '35%'};
+    box-shadow: 0px 0px 5px rgba(0,0,0,0.27);
     border-radius: 10px;
     margin: 10px;
+    background-color: #fff;
 `
+
+export const Card = ({ title, children, w }) =>  
+    <CardStyled w={w}>
+        <Container justify='column'>
+            <Container justify='flex-start'>
+                <Text size='13px' weight='900'>{title}</Text>
+            </Container>
+        </Container>
+        {children}
+    </CardStyled>
