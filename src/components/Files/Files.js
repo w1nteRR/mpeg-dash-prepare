2636766e07aca28a6ca38ae1ed8dce94f9737b71
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-12 22:44:57 +0300

    files component refactored

--- src/components/Files/Files.js
+++ src/components/Files/Files.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { mdiDelete } from '@mdi/js'
 
 import { Card } from '../shared/styled/Card'
 import { FileCard } from '../shared/styled/File.card'
+
+import { Container } from '../shared/layout'
+import { ActiveText } from '../shared/typography'
+import { error } from '../shared/colors'
 
 import { deleteFile } from '../../redux/actions/fileActions'
 
@@ -16,11 +21,20 @@ export const Files = ({ fileName }) => {
             {
                 convertedFiles.map(file => 
                     <FileCard 
-                        name={file} 
+                        name={file}
+                        icon={mdiDelete} 
                         key={file}
+                        iconColor={error}
                         onClick={() => dispatch(deleteFile(file, fileName))} 
                     />
                 )
+            }
+            {
+                !convertedFiles.length
+                &&
+                <Container m='10px 0'>
+                    <ActiveText color={error}>No files or directory</ActiveText>
+                </Container>
             }
         </Card>
     )
