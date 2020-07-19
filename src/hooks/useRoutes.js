00921f9fd3f34f82d701859ddf301ee203ed14bd
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-19 16:12:48 +0300

    routes added

--- src/hooks/useRoutes.js
+++ src/hooks/useRoutes.js
@@ -4,13 +4,27 @@ import { ConnectedRouter } from 'connected-react-router'
 
 import { Info } from '../pages/Info'
 import { Main } from '../pages/Main'
+import { Ffmpeg } from '../pages/Ffmpeg'
+import { Gpac } from '../pages/Gpac'
+
+import { NavBar } from '../components/Navigation/Nav.bar'
+
+const MainStack = () =>
+    <>
+    <NavBar />
+    <Switch>
+        <Route path='/main/info' component={Info} />
+        <Route path='/main/ffmpeg' component={Ffmpeg} />
+        <Route path='/main/gpac' component={Gpac} />
+    </Switch>
+    </>
 
 export const useRoutes = history => {
     return (
         <ConnectedRouter history={history}>
             <Switch>           
                 <Route exact path='/' component={Main} />
-                <Route path='/info' component={Info} />
+                <Route path='/main' component={MainStack} />
             </Switch>
         </ConnectedRouter>
     )
