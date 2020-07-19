a8d841404766c2cb9324713c1118a3c45a3d312a
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-19 22:41:18 +0300

    prop list refactored

--- src/components/Info/Properties.list.js
+++ src/components/Info/Properties.list.js
@@ -3,12 +3,20 @@ import React from 'react'
 import { Container } from '../shared/layout'
 import { ActiveText, Text } from '../shared/typography'
 
-export const PropertiesList = ({ properties }) => 
-    properties.map((item, index) => 
-        <Container justify='space-between' m='10px' key={index}>
-            <ActiveText>{Object.keys(item)}</ActiveText>
-            <Text size='10px' weight='700'>{Object.values(item)}</Text>
-        </Container>
-    )
+export const PropertiesList = ({ properties }) => {
+
+    const items = []
+
+    for(const [key, value] of Object.entries(properties)) {
+        items.push( 
+            <Container justify='space-between' m='10px' key={key}>
+                <ActiveText>{key}</ActiveText>
+                <Text size='10px' weight='700'>{value}</Text>
+            </Container>
+        )
+    }
+
+    return <>{items}</>
+}
     
     
